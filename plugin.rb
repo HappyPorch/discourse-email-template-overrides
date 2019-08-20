792a97d53c3ac771d88cc8b02fbc89a0031983cd
Tom van Enckevort <tom@happyporch.com>
2019-08-20 14:54:59 +0100

    Check if override file exists before loading.

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-email-template-overrides
 # about: Plugin that overrides the default notifications email templates and styles in Discourse. 
-# version: 0.1.0
+# version: 0.1.1
 # authors: HappyPorch
 # url: https://github.com/HappyPorch/discourse-email-template-overrides/
 
@@ -19,14 +19,18 @@ after_initialize do
     end
     
     # add custom email styling
-    email_style_overrides = YAML.load_file(File.expand_path("../../../public/uploads/default/original/1X/email_template_overrides/email_style_overrides.yml", __FILE__))
+    email_style_overrides_path = File.expand_path("../../../public/uploads/default/original/1X/email_template_overrides/email_style_overrides.yml", __FILE__)
     
-    require_dependency 'email/styles'
-    Email::Styles.register_plugin_style do |doc|
-        if (email_style_overrides)
-            email_style_overrides['email_style_overrides'].each do |style_selector, style|
-                doc.css(style_selector).each do |element|
-                    element['style'] = style
+    if (File.file?(email_style_overrides_path))
+        email_style_overrides = YAML.load_file(email_style_overrides_path)
+        
+        require_dependency 'email/styles'
+        Email::Styles.register_plugin_style do |doc|
+            if (email_style_overrides)
+                email_style_overrides['email_style_overrides'].each do |style_selector, style|
+                    doc.css(style_selector).each do |element|
+                        element['style'] = style
+                    end
                 end
             end
         end
